a12bfa86460e575b20bedc8e3ed30d2a2f003f44
meep2002 <fashionoldpipes@gmail.com>
2026-03-06 22:12:47 -0500

    i like made some kind of like basis or something idk
    i like coded like movement i guess

--- game/main.lua
+++ game/main.lua
@@ -5,16 +5,45 @@ local runtimeLoader = require("runtime.loader")
 function love.load()
   https = runtimeLoader.loadHTTPS()
   -- Your game load here
+  image = love.graphics.newImage("test.png")
+  love.graphics.setNewFont(12)
+  love.graphics.setColor(0,0,0)
+  love.graphics.setBackgroundColor(255,255,255)
   overlayStats.load() -- Should always be called last
+  imgx = 50 -- ok im like assigning the variables i think idk ive only used pythion
+  imgy = 50
+  speed = 200
 end
 
 function love.draw()
   -- Your game draw here
+
+  love.graphics.draw(image, imgx, imgy)
+  love.graphics.print("i dont know how to use love2d help", 400, 100)
+  love.graphics.print("wait this isnt as hard as i thought nvm", 450, 160)
+
   overlayStats.draw() -- Should always be called last
 end
 
 function love.update(dt)
   -- Your game update here
+  if shift_down then
+    speed = 400
+  else
+    speed = 200
+  end
+  if w_down then
+    imgy = imgy - speed * dt
+  end
+  if s_down then
+    imgy = imgy + speed * dt
+  end
+  if a_down then
+    imgx = imgx - speed * dt
+  end
+  if d_down then
+    imgx = imgx + speed * dt
+  end
   overlayStats.update(dt) -- Should always be called last
 end
 
@@ -22,7 +51,40 @@ function love.keypressed(key)
   if key == "escape" and love.system.getOS() ~= "Web" then
     love.event.quit()
   else
+  if key == "w" or key == "up" then
+    w_down = true
+  end
+  if key == "a" or key == "left" then
+    a_down = true
+  end
+  if key == "s" or key == "down" then
+    s_down = true
+  end
+  if key == "d" or key == "right" then
+    d_down = true
+  end
+  if key == "rshift" or key == "lshift" then
+    shift_down = true
+  end
     overlayStats.handleKeyboard(key) -- Should always be called last
+  end
+end
+
+function love.keyreleased(key)
+   if key == 'w' or key == "up" then
+      w_down = false
+   end
+  if key == "a" or key == "left" then
+    a_down = false
+  end
+  if key == 's' or key == "down" then
+      s_down = false
+   end
+  if key == "d" or key == "right" then
+    d_down = false
+  end
+  if key == "rshift" or key == "lshift" then
+    shift_down = false
   end
 end
 
